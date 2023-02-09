1922d1bdc5849b9e2262d9ca9494b07bd66e19f7
seoyeomi <seoyeonii@ajou.ac.kr>
2023-02-09 19:06:09 +0900

    7.0 finish

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,65 @@
-import { useState, useEffect } from "react";
-
-function Hello() {
-  useEffect(() => {
-    console.log("created!");
-  }, []);
-  return <h1>Hello</h1>;
-}
-
-function Bye() {
-  useEffect(() => {
-    console.log("GoodBye~");
-  }, []);
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  };
+  console.log(toDos);
   return (
     <div>
-      {showing ? <Hello /> : <Bye />}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos {toDos.length}</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
 
 export default App;
+// import { useState, useEffect } from "react";
 
+// 2nd
+// function Hello() {
+//   useEffect(() => {
+//     console.log("created!");
+//   }, []);
+//   return <h1>Hello</h1>;
+// }
+
+// function Bye() {
+//   useEffect(() => {
+//     console.log("GoodBye~");
+//   }, []);
+// }
+
+// function App() {
+//   const [showing, setShowing] = useState(false);
+//   const onClick = () => setShowing((prev) => !prev);
+//   return (
+//     <div>
+//       {showing ? <Hello /> : <Bye />}
+//       <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// 1st
 // import Button from "./Button";
 // import styles from "./App.module.css";
 
